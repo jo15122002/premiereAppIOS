abb39842a6daa111650921d9f64ef7b743622889
Digital <digital@BASS-C17.local>
2022-11-28 14:50:27 +0100

    Ajout d'un 2e bouton

--- MaPremiereApp/ViewController.swift
+++ MaPremiereApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        self.monLabel.text = "Oh pinaise il à cliqué";
+        print("cliqué");
+    }
+    
+    @IBAction func button2Clicked(_ sender: Any) {
+        if let i = self.monLabel.text{
+            print(i)
+        }
+    }
 }
 
